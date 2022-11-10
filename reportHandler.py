c4e8087f93e2eb6ae004995c5dcc5580e397f6bf
daveA420 <97268192+daveA420@users.noreply.github.com>
2022-11-09 17:20:52 -0800

    Update reportHandler.py

--- reportHandler.py
+++ reportHandler.py
@@ -2,7 +2,7 @@
 #file is used to make my report
 #doesn't carry over from last report
 import newParser #used to tokenize content of website
-from urllib.parse import urldefrag
+from urllib.parse import urldefrag, urlparse
 import shelve
 
 MYREPORT = open("myCrawlerReport.txt", 'w+')
@@ -12,18 +12,21 @@ MOSTWORDS = 0
 MOSTWORDSURL = ''
 NUMUNIQUEPAGES = 0
 UNIQUEPAGES = shelve.open('UniquePages.shelve')
+UNIQUESUBDOMAINS = shelve.open('uniqueDomains.shelve')
 
 def addToReport(url, contents):
     myTokens = newParser.tokenizeText(contents)
     myFrequencies = newParser.computeWordFrequencies(myTokens)
+    sub = urlparse(url).netloc
     urlWithoutFrag= urldefrag(url)
-    updateReport(urlWithoutFrag, myFrequencies)
+    updateReport(urlWithoutFrag, myFrequencies, sub)
 
-def updateReport(url, tokenFrequencyDict):
+def updateReport(url, tokenFrequencyDict, subdomain):
     global NUMUNIQUEPAGES
     global UNIQUEPAGES
     global MOSTWORDS
     global MOSTWORDSURL
+    global UNIQUESUBDOMAINS
 
     UNIQUEPAGES[str(url)] = True
     NUMUNIQUEPAGES = len(UNIQUEPAGES)
@@ -31,13 +34,17 @@ def updateReport(url, tokenFrequencyDict):
     if len(tokenFrequencyDict) > MOSTWORDS:
         MOSTWORDSURL = str(url)
         MOSTWORDS = len(tokenFrequencyDict)
+    if subdomain not in UNIQUESUBDOMAINS:
+        UNIQUESUBDOMAINS[str(subdomain)] = 0
+    else:
+        UNIQUESUBDOMAINS[str(subdomain)] += 1
     try:
         MYREPORT = open("myCrawlerReport.txt", 'w+')
 
         MYREPORT.write("most words: " + str(MOSTWORDS) + '\n')
         MYREPORT.write("url with most words: " + MOSTWORDSURL+ '\n')
         MYREPORT.write("number of unique pages: " + str(NUMUNIQUEPAGES) + '\n')
-        MYREPORT.write("reached the end")
+        MYREPORT.write(str([sub + '\n' for sub in UNIQUESUBDOMAINS]))
         MYREPORT.close()
     except:
             print('file Error')
